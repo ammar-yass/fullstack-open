ac06ede72bc1c1573443894aca5d9a1b2a227d9f
Ammar Yasser <plus.ammaryasser@gmail.com>
2025-03-30 06:39:09 +0200

    Implement Exercises 2.16.-2.17.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,32 +3,55 @@ import React from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
-import phonebookService from "./services/phonebook"
+import phonebookService from "./services/phonebook";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewphone] = useState("");
   const [filterText, SetFilterText] = useState("");
+  const [notification, setNotification] = useState({
+    message: null,
+    isSuccess: false,
+  });
 
-  useEffect(()=> { getAll() }, [])
+  useEffect(() => {
+    getAll();
+  }, []);
 
   const addContact = (event) => {
     event.preventDefault();
     const exisitingPerson = persons.find((person) => person.name === newName);
-    console.log("add contect")
+    console.log("add contect");
     if (exisitingPerson && newPhone === exisitingPerson.number) {
-      console.log("exisitingPerson", exisitingPerson)
+      console.log("exisitingPerson", exisitingPerson);
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    if(exisitingPerson && newPhone !== exisitingPerson.number && confirm(`${exisitingPerson.name} is already added to phonebook, replace the old number with a new pne?`)) { 
-      console.log("exisitingPerson diff phone numbber", exisitingPerson)
-      let newPeron = {...exisitingPerson, number: newPhone}
-      phonebookService.updateContact(exisitingPerson.id, newPeron).then(( _ => getAll()))
+    if (
+      exisitingPerson &&
+      newPhone !== exisitingPerson.number &&
+      confirm(
+        `${exisitingPerson.name} is already added to phonebook, replace the old number with a new one?`
+      )
+    ) {
+      console.log("exisitingPerson diff phone numbber", exisitingPerson);
+      let newPeron = { ...exisitingPerson, number: newPhone };
+      phonebookService
+        .updateContact(exisitingPerson.id, newPeron)
+        .then((_) => {
+          showNotification(`added ${newPeron.name}`, true)
+          return getAll();
+        });
       return;
     }
-    phonebookService.createContact({ name: newName, number: newPhone }).then(( _ => getAll()))
+    phonebookService
+      .createContact({ name: newName, number: newPhone })
+      .then((_) => {
+        showNotification(`added ${newName}`, true)
+        return getAll();
+      });
     setNewName("");
     setNewphone("");
   };
@@ -38,18 +61,35 @@ const App = () => {
     SetFilterText(filterText);
   };
 
-  const deletePerson = (id) => { 
-    const person = persons.find((person) => person.id === id)
-    if(confirm(`delete ${person.name}?`))
-      phonebookService.deletePerson(id).then( _ => getAll())
-  }
+  const deletePerson = (id) => {
+    const person = persons.find((person) => person.id === id);
+    if (confirm(`delete ${person.name}?`))
+      phonebookService
+        .deletePerson(id)
+        .then((_) => getAll())
+        .catch(() =>
+          showNotification(
+            `infromation of ${person.name} has already been removed from server`,
+            false
+          )
+        );
+  };
 
-  const getAll = () => { 
+  const getAll = () => {
     phonebookService.getAll().then((response) => {
       console.log(response);
       return setPersons(response);
-    })
-  }
+    });
+  };
+
+  const showNotification = (message, isSuccess) => {
+    console.log(message, isSuccess);
+    
+    setNotification({ message, isSuccess });
+    setTimeout(() => {
+      setNotification({ message: null, isSuccess: true });
+    }, 5000);
+  };
 
   const displayedPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(filterText.trim().toLowerCase())
@@ -58,6 +98,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification
+        message={notification.message}
+        isSuccess={notification.isSuccess}
+      />
       <Filter filterText={filterText} onFilterChange={setFilter} />
       <h3>Add a new</h3>
       <PersonForm
@@ -68,7 +112,7 @@ const App = () => {
         onSubmit={addContact}
       />
       <h3>Numbers</h3>
-      <Persons persons={displayedPersons} onDelete={(id) => deletePerson(id)}/>
+      <Persons persons={displayedPersons} onDelete={(id) => deletePerson(id)} />
     </div>
   );
 };
---
+++ part2/phonebook/src/components/Notification.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Notification = ({ message, isSuccess }) => {
+  if (!message) return null;
+
+  return (
+    <div className={isSuccess ? "success" : "error"}>
+      {message}
+    </div>
+  );
+};
+
+export default Notification;
